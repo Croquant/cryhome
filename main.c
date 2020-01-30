4dca2dd9a0c69d8a2be9d1666e7fd9b21c531fbf
Maxime Desmet Vanden Stock <maximedesmet@yahoo.com>
2020-01-29 21:05:26 -0800

    code structure

--- main.c
+++ main.c
@@ -1,25 +1,41 @@
 #include <ncurses.h>
-#include <stdio.h>
 #include <unistd.h>
 
 #define DELAY 30000
 
-#define STAR "*"
-#define SHIP "@"
+#define STAR "."
+#define SHIP ">"
 	
 int WIDTH, HEIGHT;
 
-uint32_t nProcGen = 0;
+uint32_t seed = 0;
 
 uint32_t rnd()
 {
-	nProcGen += 0xe120fc15;
+	seed += 0xe120fc15;
 	uint64_t tmp;
-	tmp = (uint64_t)nProcGen * 0x4a39b70d;
+	tmp = (uint64_t)seed * 0x4a39b70d;
 	uint32_t m1 = (tmp >> 32) ^ tmp;
 	tmp = (uint64_t)m1 * 0x12fad5c9;
 	uint32_t m2 = (tmp >> 32) ^ tmp;
 	return m2;
+}
+
+void drawMap (int posX, int posY)
+{
+	for (int x = 0; x < WIDTH; x++) {
+		for (int y = 0; y < HEIGHT; y++) {
+			seed = (y + posY) << 16 | (x + posX);
+			mvprintw(y, x, rnd() % 256 > 1 ? " " : STAR);
+		}
+	}
+}
+
+void drawDebug (int posX, int posY)
+{
+	char debug[50];
+	snprintf(debug, 50, "X: %u, Y: %u", posX, posY);
+	mvprintw(0, 0, debug);(0, 0, debug);
 }
 
 int main (int argc, char *argv[])
@@ -28,45 +44,41 @@ int main (int argc, char *argv[])
 	noecho();
 	curs_set(FALSE);
 
-	getmaxyx(stdscr, HEIGHT, WIDTH);
-
-	int16_t xOffset = 0;
-	int16_t yOffset = 0;
+	int16_t playerX = 1320;
+	int16_t playerY = 1320;
 
 	char in = '0';
 
 	while (1) {
+		//DRAW LOOP
+		getmaxyx(stdscr, HEIGHT, WIDTH);
 		clear();
-		for (int x = 0; x < WIDTH; x++) {
-			for (int y = 0; y < HEIGHT; y++) {
-				nProcGen = (y + yOffset) << 16 | (x + xOffset);
-				mvprintw(y, x, rnd() % 256 > 8 ? " " : STAR);
-			}
-		}
-		char debug[50];
-		snprintf(debug, 50, "X: %u, Y: %u", xOffset, yOffset);
-		mvprintw(0, 0, debug);(0, 0, debug);
+
+		drawMap(playerX, playerY);
+
+		drawDebug(playerX, playerY);
+		
 		mvprintw(HEIGHT / 2, WIDTH / 2, SHIP);
 		refresh();
 
+		//INPUT LOOP
 		in = getchar();
-		if (in == 'd' || in == 'C') {
-			xOffset += 1;
+		if (in == 'f' || in == 'C') {
+			playerX += 1;
 		}
-		if (in == 'a' || in == 'D') {
-			xOffset -= 1;
+		if (in == 's' || in == 'D') {
+			playerX -= 1;
 		}
-		if (in == 's' || in == 'B') {
-			yOffset += 1;
+		if (in == 'd' || in == 'B') {
+			playerY += 1;
 		}
-		if (in == 'w' || in == 'A') {
-			yOffset -= 1;
+		if (in == 'e' || in == 'A') {
+			playerY -= 1;
 		}
 		if (in == 'q') {
 			break;
 		}
 		usleep(DELAY);
-		getmaxyx(stdscr, HEIGHT, WIDTH);
 	}
 
 	endwin();
